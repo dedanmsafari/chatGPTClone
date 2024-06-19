ebe7f5fceee3041422c368e14a9502b259fda0ef
dedanmsafari <dedan.msafari@gmail.com>
2024-06-19 21:27:09 +0300

    layout file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,14 @@
 import { Ionicons } from "@expo/vector-icons";
-import { Stack, router } from "expo-router";
+import { Stack, router, SplashScreen } from "expo-router";
 import { TouchableOpacity } from "react-native";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Constants from "expo-constants";
 
-export default function RootLayout() {
+import tokenCache from "@/utils/clerkTokenCache";
+export { ErrorBoundary } from "@/utils/errorBoundary";
+
+function InitialLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
@@ -21,5 +27,18 @@ export default function RootLayout() {
         }}
       />
     </Stack>
+  );
+}
+
+export default function RootLayOut() {
+  return (
+    <ClerkProvider
+      publishableKey={Constants.expoConfig?.extra?.clerkPublishableKey}
+      tokenCache={tokenCache}
+    >
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <InitialLayout />
+      </GestureHandlerRootView>
+    </ClerkProvider>
   );
 }
