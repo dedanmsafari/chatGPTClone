c9cee1f6b037bf74650ab23f04a236c3c0c57626
dedanmsafari <dedan.msafari@gmail.com>
2024-06-22 00:03:28 +0300

    index layOut and new page

---
+++ app/(root)/(drawer)/(chat)/new.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const newChat = () => {
+  return (
+    <View>
+      <Text>New Chat</Text>
+    </View>
+  );
+};
+
+export default newChat;
