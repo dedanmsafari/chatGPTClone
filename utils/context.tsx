02fb52ae623118756f1e46d0b3c705f313b2b7ea
dedanmsafari <dedan.msafari@gmail.com>
2024-06-21 10:07:00 +0300

    use storage and context

---
+++ utils/context.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+type CreateContext = <T extends object>() => readonly [
+  () => T,
+  React.Provider<T | null>
+];
+
+export const createContext: CreateContext = <T extends object>() => {
+  const Context = React.createContext<T | null>(null);
+
+  function useContext() {
+    const ctx = React.useContext(Context);
+
+    if (!ctx) {
+      throw new Error("Cannot use context outside the Provider");
+    }
+
+    return ctx;
+  }
+
+  return [useContext, Context.Provider] as const;
+};
