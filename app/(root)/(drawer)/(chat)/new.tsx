daab3c79b19c86f65bdcc9abb8f0d30e2b82ea7c
dedanmsafari <dedan.msafari@gmail.com>
2024-06-25 08:51:21 +0300

    new chtat page

--- app/(root)/(drawer)/(chat)/new.tsx
+++ app/(root)/(drawer)/(chat)/new.tsx
@@ -1,13 +1,25 @@
-import { View, Text, Button, Platform } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  Platform,
+  KeyboardAvoidingView,
+  ScrollView,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { defaultStyles } from "@/constants/Styles";
 import { Stack, useNavigation } from "expo-router";
 import { useSession } from "@/context/authContext";
 import HeaderDropDown from "@/components/HeaderDropDown";
+import MessageInput from "@/components/MessageInput";
 
 const Page = () => {
   const { signOut } = useSession();
   const [gptVersion, setGptVersion] = useState("3.5");
+
+  const getCompletion = (message: string) => {
+    console.log("Completion message :", message);
+  };
 
   return (
     <View style={defaultStyles.pageContainer}>
@@ -29,8 +41,28 @@ const Page = () => {
           ),
         }}
       />
-      <Text>Home </Text>
-      <Button title="Sign Out" onPress={signOut} />
+      <View style={{ flex: 1 }}>
+        <Text>Home </Text>
+
+        <Button title="Sign Out" onPress={signOut} />
+        {/* <ScrollView>
+          {Array.from({ length: 100 }).map((_, index) => (
+            <Text key={index}>{index}njkkjnjknjknjklnk</Text>
+          ))}
+        </ScrollView> */}
+      </View>
+      <KeyboardAvoidingView
+        keyboardVerticalOffset={70}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          width: "100%",
+        }}
+      >
+        <MessageInput onShouldSendMessage={getCompletion} />
+      </KeyboardAvoidingView>
     </View>
   );
 };
