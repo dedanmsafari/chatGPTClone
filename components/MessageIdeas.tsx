01a27fb8f14faa7769c096ada2030bf776de8587
dedanmsafari <dedan.msafari@gmail.com>
2024-06-26 14:45:36 +0300

    message ideas

---
+++ components/MessageIdeas.tsx
@@ -0,0 +1,59 @@
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import React from "react";
+import Colors from "@/constants/Colors";
+
+type MessageProp = {
+  onSelectCard: (message: string) => void;
+};
+
+const PredefinedMessages = [
+  { title: "Explain React Native", text: "like I'm five years old" },
+  {
+    title: "Suggest fun activities",
+    text: "for a family visiting Taita Taveta",
+  },
+  { title: "Recommend a dish", text: "to impress a date who's a picky eater" },
+];
+
+const MessageIdeas = ({ onSelectCard }: MessageProp) => {
+  return (
+    <View>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingHorizontal: 20,
+          paddingVertical: 10,
+          gap: 16,
+        }}
+      >
+        {PredefinedMessages.map((message, index) => (
+          <TouchableOpacity
+            key={index}
+            style={styles.card}
+            onPress={() => onSelectCard(`${message.title} ${message.text}`)}
+          >
+            <Text>{message.title}</Text>
+            <Text>{message.text}</Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default MessageIdeas;
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: Colors.input,
+    padding: 14,
+    borderRadius: 10,
+  },
+});
