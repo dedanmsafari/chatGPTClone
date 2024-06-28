087f84cb47aa4ea54fe371af9fb34299b05401e0
dedanmsafari <dedan.msafari@gmail.com>
2024-06-28 11:47:45 +0300

    added modal layout

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,8 +1,10 @@
-import { View, Text, ActivityIndicator } from "react-native";
+import { View, Text, ActivityIndicator, TouchableOpacity } from "react-native";
 import React from "react";
 
 import { useSession } from "@/context/authContext";
-import { Redirect, Stack } from "expo-router";
+import { Redirect, Stack, router } from "expo-router";
+import Colors from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
 
 const AppLayout = () => {
   const { session } = useSession();
@@ -14,7 +16,29 @@ const AppLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
-      <Stack.Screen name="(modal)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="(modal)/settings"
+        options={{
+          presentation: "modal",
+          headerTitle: "Settings",
+          headerTitleAlign: "center",
+          headerShadowVisible: false,
+          headerStyle: { backgroundColor: Colors.selected },
+          headerRight: () =>
+            router.canGoBack() && (
+              <TouchableOpacity
+                onPress={() => router.back()}
+                style={{
+                  backgroundColor: Colors.greyLight,
+                  borderRadius: 20,
+                  padding: 6,
+                }}
+              >
+                <Ionicons name="close-outline" size={16} color={Colors.grey} />
+              </TouchableOpacity>
+            ),
+        }}
+      />
     </Stack>
   );
 };
