b633646a17d470e116afa14ddf23c37f4b7bb67b
dedanmsafari <dedan.msafari@gmail.com>
2024-07-24 20:53:49 +0300

    layout changes

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,12 +1,13 @@
-import { View, Text, ActivityIndicator, TouchableOpacity } from "react-native";
-import React from "react";
-import { SQLiteProvider } from "expo-sqlite";
+import { View, Text, ActivityIndicator, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { SQLiteProvider } from 'expo-sqlite';
 
-import { useSession } from "@/context/authContext";
-import { Redirect, Stack, router } from "expo-router";
-import Colors from "@/constants/Colors";
-import { Ionicons } from "@expo/vector-icons";
-import { migrateDbIfNeeded } from "@/utils/Database";
+import { useSession } from '@/context/authContext';
+import { Redirect, Stack, router } from 'expo-router';
+import Colors from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
+import { migrateDbIfNeeded } from '@/utils/Database';
+import RevenueCatProvider from '@/providers/RevenueCat';
 
 const AppLayout = () => {
   const { session } = useSession();
@@ -16,62 +17,64 @@ const AppLayout = () => {
   }
 
   return (
-    <SQLiteProvider databaseName="chat.db" onInit={migrateDbIfNeeded}>
-      <Stack>
-        <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="(modal)/settings"
-          options={{
-            presentation: "modal",
-            headerTitle: "Settings",
-            headerTitleAlign: "center",
-            headerShadowVisible: false,
-            headerStyle: { backgroundColor: Colors.selected },
-            headerRight: () =>
-              router.canGoBack() && (
+    <RevenueCatProvider>
+      <SQLiteProvider databaseName="chat.db" onInit={migrateDbIfNeeded}>
+        <Stack>
+          <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="(modal)/settings"
+            options={{
+              presentation: 'modal',
+              headerTitle: 'Settings',
+              headerTitleAlign: 'center',
+              headerShadowVisible: false,
+              headerStyle: { backgroundColor: Colors.selected },
+              headerRight: () =>
+                router.canGoBack() && (
+                  <TouchableOpacity
+                    onPress={() => router.back()}
+                    style={{
+                      backgroundColor: Colors.greyLight,
+                      borderRadius: 20,
+                      padding: 6,
+                    }}
+                  >
+                    <Ionicons
+                      name="close-outline"
+                      size={16}
+                      color={Colors.grey}
+                    />
+                  </TouchableOpacity>
+                ),
+            }}
+          />
+          <Stack.Screen
+            name="(modal)/Image/[url]"
+            options={{
+              presentation: 'fullScreenModal',
+              headerTitle: '',
+              headerBlurEffect: 'dark',
+              headerTransparent: true,
+
+              headerTitleAlign: 'center',
+              headerShadowVisible: false,
+              headerStyle: { backgroundColor: 'rgba(0,0,0,0.4)' },
+              headerLeft: () => (
                 <TouchableOpacity
                   onPress={() => router.back()}
                   style={{
-                    backgroundColor: Colors.greyLight,
                     borderRadius: 20,
-                    padding: 6,
+                    padding: 4,
                   }}
                 >
-                  <Ionicons
-                    name="close-outline"
-                    size={16}
-                    color={Colors.grey}
-                  />
+                  <Ionicons name="close-outline" size={28} color={'#fff'} />
                 </TouchableOpacity>
               ),
-          }}
-        />
-        <Stack.Screen
-          name="(modal)/Image/[url]"
-          options={{
-            presentation: "fullScreenModal",
-            headerTitle: "",
-            headerBlurEffect: "dark",
-            headerTransparent: true,
-
-            headerTitleAlign: "center",
-            headerShadowVisible: false,
-            headerStyle: { backgroundColor: "rgba(0,0,0,0.4)" },
-            headerLeft: () => (
-              <TouchableOpacity
-                onPress={() => router.back()}
-                style={{
-                  borderRadius: 20,
-                  padding: 4,
-                }}
-              >
-                <Ionicons name="close-outline" size={28} color={"#fff"} />
-              </TouchableOpacity>
-            ),
-          }}
-        />
-      </Stack>
-    </SQLiteProvider>
+            }}
+          />
+        </Stack>
+      </SQLiteProvider>
+    </RevenueCatProvider>
   );
 };
 
